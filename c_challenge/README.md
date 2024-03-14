00da6180ad58ecfa2238b21e6cb6b67e49b744d8
Abraham Livinus <abrahamlivinus@gmail.com>
2024-03-14 13:33:42 -0700

    c_hello_world.c

---
+++ c_challenge/README.md
@@ -0,0 +1,4 @@
+This Folder / Directory contains files, findings, researchs and projects on
+the 100 days of ALXSE challenge
+
+Author: Abraham Livinus.
---
+++ c_challenge/c_hello_world.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * hello_world - this function prints the first C hello world.
+ */
+
+void hello_world(void)
+{
+	printf("Abraham Livinus Says Hello World On Day 2 of the ");
+	printf("#100daysofALXSE challenge with C language.\n");
+}
+
+/**
+ * main - This script handles the main function that
+ * prints C hello world.
+ *
+ * hello_world - this function prints the first C hello world.
+ *
+ * Return: Always returns 0.
+ */
+
+int main(void)
+{
+	hello_world();
+	return (0);
+}
