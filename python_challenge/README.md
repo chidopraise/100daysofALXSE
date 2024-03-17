eff29988daadd831ced012c3a69f292ebaeb0ae9
Abraham Livinus <abrahamlivinus@gmail.com>
2024-03-17 12:18:32 -0700

    python_hello_world.py

---
+++ python_challenge/README.md
@@ -0,0 +1,4 @@
+This Directory / Folder contains files and projects on the 100daysofALXSE challenge.
+python_challenge
+
+Author: Abraham Livinus.
---
+++ python_challenge/python_hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''
+    This script contain a function that prints hello world in python.
+'''
+
+
+def hello_world():
+    print("Abraham Livinus says Hello World in Python language")
+
+
+hello_world()
